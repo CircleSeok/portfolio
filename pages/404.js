fc0cc8b46bd72ff6a51f5a77980561d8477373d7
Wonseok <dnjstjr970302@gamil.com>
2023-07-24 15:39:34 +0900

    next-porfolio

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function pageNotFound() {
+  return (
+    <>
+      <h1>페이지를 찾을수 없습니다</h1>
+    </>
+  );
+}
---
+++ pages/projects.js
@@ -0,0 +1,10 @@
+import Layout from '@/components/Layout';
+import React from 'react';
+
+export default function Projects() {
+  return (
+    <Layout>
+      <h1>프로젝트</h1>
+    </Layout>
+  );
+}
