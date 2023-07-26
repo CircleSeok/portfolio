9296a7a4283085520756bf074a4003eb89386e50
Wonseok <dnjstjr970302@gamil.com>
2023-07-27 07:15:56 +0900

    feat/노션 api 연결

--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,11 @@
 import Layout from '@/components/Layout';
 import Head from 'next/head';
 import React from 'react';
+import { TOKEN, DATABASE_ID } from '@/config';
 
-export default function Projects() {
+export default function Projects({ projects }) {
+  console.log(projects);
+
   return (
     <Layout>
       <Head>
@@ -11,7 +14,51 @@ export default function Projects() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1>프로젝트</h1>
+      <h1>총 프로젝트 : {projects.results.length}</h1>
+
+      {projects.results.map((aProject) => (
+        <h1>{aProject.properties.Name.title[0].plain_text}</h1>
+      ))}
     </Layout>
   );
+}
+
+//빌드 타임에 호출
+
+export async function getStaticProps() {
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Notion-Version': '2022-02-22',
+      'content-type': 'application/json',
+      authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({
+      sorts: [
+        {
+          property: 'Name',
+          direction: 'ascending',
+        },
+      ],
+      page_size: 100,
+    }),
+  };
+
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
+
+  const projects = await res.json();
+
+  const projectNames = projects.results.map(
+    (aProject) => aProject.properties.Name.title[0]?.plain_text
+  );
+
+  console.log(`projectNames': ${projectNames}`);
+
+  return {
+    props: { projects },
+  };
 }
